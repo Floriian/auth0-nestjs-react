468771b4d2b5fd07f262b6385f2d2346633808b0
Floriian <lovasifloriian@outlook.hu>
2023-09-18 18:21:23 +0200

    generate usersmodule.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync(typeormConfig),
     ConfigModule.forRoot(),
     GraphQLModule.forRoot(apolloConfig),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
